98f18c7be3f8bdb0d19af769ebb583f1a80db92f
Akash Kumar Prasad <akash22gupta33@gmail.com>
2022-01-11 22:12:44 +0530

    project update

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -1,73 +1,75 @@
-import css1 from '../img/portImages/cv_css.png';
-import css2 from '../img/portImages/ms.png';
-import react1 from '../img/portImages/react_styled.png';
-import img2 from '../img/portImages/maya-4.jpg';
-import img3 from '../img/portImages/maya-3.jpg';
+import Udemy_1 from '../img/portImages/Udemy_1.png';
+import Udemy_2 from '../img/portImages/udemy_2.png';
+import udemy_3 from '../img/portImages/udemy_3.png';
+import saphora from '../img/portImages/saphora.png';
+
 
 const portfolios = [
     {
-        id: 1,
-        category: 'CSS',
-        image: css1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Responsive Resume',
-        text: 'Created with only HTML and CSS.'
-    },
-    {
         id: 2,
-        category: 'Python',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
+        category: 'Udemy 3.0',
+        image: udemy_3,
+        link1: 'https://github.com/KumarAkash22/Udemy-Clone-Backend',
+        link2: 'https://aqueous-fortress-78543.herokuapp.com/',
+        title: 'Udemy Website 3.0',
+        text: 'Create with Frontend Backend'
     },
     {
-        id: 3,
-        category: 'Javascript',
-        image: img3,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Animate In Maya',
-        text: 'Number one Animation Application'
-    },
-    {
-        id: 4,
-        category: 'Animation',
-        image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
-    },
-    {
-        id: 5,
-        category: 'Animation',
-        image: img2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'How To Use Blender',
-        text: 'Free Animation Software'
+        id: 1,
+        category: 'Udemy 2.0',
+        image: Udemy_2,
+        link1: 'https://github.com/KumarAkash22/udemy-clone-1',
+        link2: 'https://aqueous-fortress-78543.herokuapp.com/',
+        title: 'Udemy 2.0',
+        text: 'Create with HTML Tailwind and Mock Server'
     },
     {
         id: 6,
-        category: 'React JS',
-        image: react1,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Responsive Portfolio Website',
-        text: 'Created using different technologies such as Material UI, Styled-Components and more...'
+        category: 'Udemy 1.0',
+        image: Udemy_1,
+        link1: 'https://github.com/KumarAkash22/Udemy-Clone',
+        link2: 'https://kapoorkartik.github.io/Udemy-Clone/home_page.html',
+        title: 'Udemy website 1.0',
+        text: 'Create with HTML CSS and Advance JavaScript'
     },
+    
     {
-        id: 7,
-        category: 'CSS',
-        image: css2,
-        link1: 'https://www.google.com',
-        link2: 'https://www.google.com',
-        title: 'Microsoft Website Clone',
-        text: 'Created using HTML and CSS'
+        id: 3,
+        category: 'Sephora',
+        image: saphora,
+        link1: 'https://github.com/KumarAkash22/Sephora-clone',
+        link2: 'https://kumarakash22.github.io/Sephora-clone/slide.html',
+        title: 'Sephora website',
+        text: 'Create with HTML and CSS and Basic JavaScript'
     }
+    // {
+    //     id: 4,
+    //     category: 'Animation',
+    //     image: img2,
+    //     link1: 'https://www.google.com',
+    //     link2: 'https://www.google.com',
+    //     title: 'How To Use Blender',
+    //     text: 'Free Animation Software'
+    // },
+    // {
+    //     id: 5,
+    //     category: 'Animation',
+    //     image: img2,
+    //     link1: 'https://www.google.com',
+    //     link2: 'https://www.google.com',
+    //     title: 'How To Use Blender',
+    //     text: 'Free Animation Software'
+    // },
+    
+    // {
+    //     id: 7,
+    //     category: 'Udemy 2.',
+    //     image: Udemy_2,
+    //     link1: 'https://github.com/KumarAkash22/udemy-clone-1',
+    //     link2: 'https://aqueous-fortress-78543.herokuapp.com/',
+    //     title: 'Udemy 2.0',
+    //     text: 'Create with HTML Tailwind and Mock Server'
+    // }
 ]
 
 export default portfolios;
